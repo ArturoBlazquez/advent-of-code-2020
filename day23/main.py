9b9b1b44b25e15812eb640005c784d5c5a65585a
Arturo Blázquez <arturo.blazquezperez@gmail.com>
2020-12-26 14:35:12 +0100

    Day 23 part 2

--- day23/main.py
+++ day23/main.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 from utils import read_file, print_part_1, print_part_2
 
 # -- PARSE INPUT -- #
@@ -30,46 +28,77 @@ print_part_1(''.join([str(x) for x in cups[one_position + 1:] + cups[:one_positi
 
 
 # -- PART 2 -- #
-def move_v2(cups_list):
-    current_cup = np.where(cups_list == 0)[0][0]
-    three_cups = [np.where(cups_list == 1)[0][0], np.where(cups_list == 2)[0][0], np.where(cups_list == 3)[0][0]]
-
-    for i in range(1, 5):
-        index_to_find = (current_cup - i) % 9
-        if index_to_find in three_cups:
-            pass
-        else:
-            cut = cups_list[index_to_find]
-            break
-
-    cups_list[np.where(cups_list > cut)[0]] += 3
-    cups_list -= 4
-    cups_list[current_cup] = 8
-    cups_list[three_cups[0]] = cut - 3
-    cups_list[three_cups[1]] = cut - 2
-    cups_list[three_cups[2]] = cut - 1
+class Node:
+    def __init__(self, data):
+        self.label = data
+        self.next_cup: Node = None
+        self.previous_label_cup: Node = None
 
 
-cups_unparsed = [int(letter) for letter in read_file('input.txt')[0]]
-cups = []
-for i in range(1, 10):
-    cups.append(cups_unparsed.index(i))
-cups = np.array(cups + list(range(9, 1000000)))
+class LinkedList:
+    def __init__(self):
+        self.head: Node = None
 
 
-def print_list(cups_list):
-    for i in range(len(cups_list)):
-        print(np.where(cups_list == i)[0][0] + 1, end=' ')
+def get_cups(cups_list):
+    nodes = [Node(i) for i in range(1, 10)]
+    cups_ret = LinkedList()
+    first_cup_index = int(cups_list[0]) - 1
+    cups_ret.head = nodes[first_cup_index]
 
-    print('')
+    for i in [int(letter) for letter in cups_list[1:]]:
+        cups_ret.head.next_cup = nodes[i - 1]
+        cups_ret.head = nodes[i - 1]
+
+    cups_ret.head.next_cup = nodes[first_cup_index]
+    cups_ret.head = nodes[first_cup_index]
+
+    for i in range(9):
+        nodes[i].previous_label_cup = nodes[i - 1]
+
+    for _ in range(8):
+        cups_ret.head = cups_ret.head.next_cup
+
+    cups_ret.head.next_cup = Node(10)
+    cups_ret.head = cups_ret.head.next_cup
+    cups_ret.head.previous_label_cup = nodes[9 - 1]
+
+    for label in range(11, 1000001):
+        cups_ret.head.next_cup = Node(label)
+        cups_ret.head.next_cup.previous_label_cup = cups_ret.head
+        cups_ret.head = cups_ret.head.next_cup
+
+    cups_ret.head.next_cup = nodes[first_cup_index]
+    nodes[0].previous_label_cup = cups_ret.head
+    cups_ret.head = cups_ret.head.next_cup
+
+    return cups_ret
 
 
-for _ in range(10000):
+def move_v2(cups_list: LinkedList):
+    current_cup = cups_list.head
+    three_cups = [current_cup.next_cup, current_cup.next_cup.next_cup, current_cup.next_cup.next_cup.next_cup]
+
+    next_head = three_cups[2].next_cup
+
+    cups_list.head = next_head
+    current_cup.next_cup = next_head
+
+    previous_label_cup = current_cup.previous_label_cup
+    while previous_label_cup in three_cups:
+        previous_label_cup = previous_label_cup.previous_label_cup
+
+    cut_next = previous_label_cup.next_cup
+    previous_label_cup.next_cup = three_cups[0]
+    three_cups[2].next_cup = cut_next
+
+
+cups = get_cups(read_file('input.txt')[0])
+
+for _ in range(10000000):
     move_v2(cups)
 
-# one_position = cups.index(1)
-# print(one_position)
-# print(cups[one_position + 1])
-# print(cups[one_position + 2])
+while cups.head.label != 1:
+    cups.head = cups.head.next_cup
 
-print_part_2(cups)
+print_part_2(cups.head.next_cup.label * cups.head.next_cup.next_cup.label)
